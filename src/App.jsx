a6b74ef697157e19b3869591498e71dd6454daf7
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-01-16 12:56:40 +0530

    Added Hero Section Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto px-6 pt-20 ">
+        <HeroSection />
+      </div>
     </>
   );
 };
